38b2110e0bb0ae43008c9b11162405c840f44236
Fr3dMur <murillo.freddy2002@gmail.com>
2022-08-23 11:31:15 +0200

    adding gulp-sass and modified the gulpfile.js with basic requires for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-function tarea(done){
-    console.log('Mi primera tarea');
+// De esta manera nos traemos funcionalidades de gulp
+const {src, dest} = require("gulp");
+// src, sirve para determinar la ruta de un archivo
+// dest, es una funcion que la empleamos para almacenar algo en una carpeta de estilo
+const sass = require("sass")
 
-    done();
+
+
+function css(done) {
+
+    /*Podemos colocar .pipe() delante de otro .pipe()
+      y se ejucataran uno detras del otro */ 
+
+    src("src/scss/app.scss")// Debemos identificar el archivo de SASS
+    .pipe(sass())// Como segundo paso debemos Compilarlo 
+    .pipe(dest("build/css")); // Ultimo paso es Almacenarla en el disco duro
+
+    done(); // Es un callback que avisa a gulp cuando llegamos al final
 }
 
-exports.tarea = tarea;
+exports.css = css;
