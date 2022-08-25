b543852c6974d772effb0de2b7db38eb2988bf43
Fr3dMur <murillo.freddy2002@gmail.com>
2022-08-25 22:29:51 +0200

    Galery its allready, now i add section boletos & ivee started to set the firts channges

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ function iniciarApp(){
     crearGaleria();
 };
 
+// Agrega galeria al HTML
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
 
@@ -16,8 +17,47 @@ function crearGaleria(){
         <source srcset="../../build/img/thumb/${i}.webp" alt="Imagen de la galeria" type="image/webp">
         
         <img loading="lazy" width="200" height="300" src="../../build/img/thumb/${i}.png" alt="Imagen de la galeria">
-        `
+        `;
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        };
 
         galeria.appendChild(imagen);
     };
+}
+
+// Crea el Overlay con la imagen
+function mostrarImagen(id){
+    const imagen = document.createElement('picture')
+    imagen.innerHTML =`
+        <source srcset="../../build/img/grande/${id}.avif" alt="Imagen de la galeria" type="image/avif">
+        <source srcset="../../build/img/grande/${id}.webp" alt="Imagen de la galeria" type="image/webp">
+        
+        <img loading="lazy" width="200" height="300" src="../../build/img/grande/${id}.png" alt="Imagen de la galeria">
+    `;
+
+   const overlay = document.createElement('DIV');
+   overlay.appendChild(imagen);
+   overlay.classList.add('overlay');
+   overlay.onclick = function(){
+        overlay.remove();
+        body.classList.remove('fijar-body');
+   }
+
+   
+   // Cierra la ventana modal
+   const cerrarModal = document.createElement('P');
+   cerrarModal.textContent = 'X';
+   cerrarModal.onclick = function(){
+        overlay.remove();
+        body.classList.remove('fijar-body');
+   };
+   cerrarModal.classList.add('btn-cerrar');
+   
+   // Agregando elementos al HTML
+   const body = document.querySelector('body');
+   body.appendChild(overlay);
+   overlay.appendChild(cerrarModal);
+   body.classList.add('fijar-body');
 }
